afc3a18d64505e11fbdadc2c422d5db7a734eb1f
craigwann <craigwann@me.com>
2018-08-10 09:56:23 -0700

    initial API test w/postman

---
+++ src/doctor.js
@@ -0,0 +1,18 @@
+
+export class drService{
+  getDrByInput(input){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://www.rijksmuseum.nl/api/en/collection?q=${input}&key=${process.env.API_KEY}&format=json&imgonly=true`;
+      request.onload = function() {
+        if (this.status === 200) {
+         resolve(request.response);
+       } else {
+         reject(Error(request.statusText));
+       }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+}
---
+++ src/scratch.js
@@ -0,0 +1,3 @@
+45.520712, -122.677375
+
+https://api.betterdoctor.com/2016-03-01/doctors?location=45.520712, -122.677375,100&skip=2&limit=10&user_key=28eb1a4139c842cda8fb4f3a4039af4c
