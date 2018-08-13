aaf12179908c89b1d7f5575e4917cd66fa6add2c
craigwann <craigwann@me.com>
2018-08-12 20:39:29 -0700

    add search by doctor name

--- src/doctor.js
+++ src/doctor.js
@@ -16,13 +16,23 @@ export class DrService{
       request.open("GET", url, true);
       request.send();
     });
+  }
 
-    // promise.then(function(response) {
-    //   let keywordInfo = JSON.parse(response);
-    //   addKeywordHTML(keywordInfo);
-    // });
+  getDrByName(inputName, addDoctorHTML) {
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${inputName}&location=45.520712, -122.677375,20&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
 
 
   }
-
-}
--- src/main.js
+++ src/main.js
@@ -26,6 +26,26 @@ function addKeywordHTML(keywordInfo){
   }
 }
 
+function addDoctorHTML(doctorInfo){
+  if (doctorInfo.meta.count === 0) {
+    $('#doctorSearchResponse').append(`<p>Sorry, no doctors met your search criteria. Please <a href="index.html">try your search again</a>.</p>`);
+  } else {
+    for (var i = 0; i < doctorInfo.data.length; i++) {
+      $('#doctorSearchResponse').append(`<div class="card" style="width: 300px; height: 360px;">
+        <div class="card-body">
+          <h5 class="card-title">${doctorInfo.data[i].profile.first_name}  ${doctorInfo.data[i].profile.last_name}</h5>
+          <p class="card-text">Accepts new patients: ${doctorInfo.data[i].practices[0].accepts_new_patients}<br>
+            Address: ${doctorInfo.data[i].practices[0].visit_address.street}<br>${doctorInfo.data[i].practices[0].visit_address.city} ${doctorInfo.data[i].practices[0].visit_address.state} ${doctorInfo.data[i].practices[0].visit_address.zip}<br>
+          Phone: ${doctorInfo.data[i].practices[0].phones[0].number}<br>
+          <a href="${doctorInfo.data[i].practices[0].website}">Website</a> <br>
+          <img src="${doctorInfo.data[i].profile.image_url}"></p>
+        </div>
+      </div>`);
+      $('#doctorSearchResponse').show();
+      $(".newsearch").show();
+    }
+  }
+}
 
 
 $(document).ready(function(){
@@ -44,8 +64,23 @@ $(document).ready(function(){
       addKeywordHTML(keywordInfo);
     });
 
+    $(".queryForms").hide();
+  });
+
+  $("form#searchByName").submit(function(event) {
+    event.preventDefault();
+
+    let inputName = $("#doctorName").val();
+    let drService = new DrService;
+    let promise = drService.getDrByName(inputName, addDoctorHTML);
+
+    promise.then(function(response) {
+      let doctorInfo = JSON.parse(response);
+      addDoctorHTML(doctorInfo);
+    });
 
     $(".queryForms").hide();
 
   });
+
 });
